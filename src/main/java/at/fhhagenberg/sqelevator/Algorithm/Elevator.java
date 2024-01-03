18e783c7c392cd72fca23c3fb7e14a0039a87243
Daniel Lauß <daniel@Daniels-MacBook-Pro-5.local>
2024-01-03 12:33:58 +0100

    minor changes in elevator algo

--- src/main/java/at/fhhagenberg/sqelevator/Algorithm/Elevator.java
+++ src/main/java/at/fhhagenberg/sqelevator/Algorithm/Elevator.java
@@ -25,6 +25,8 @@ public class Elevator {
     /** The MQTT client */
     private AlgoMqttClient client;
 
+    private boolean debug = false;
+
 
     public Elevator(int id, int nrOfFloors, int maxNrPassengers, AlgoMqttClient client) {
         if (maxNrPassengers <= 0 || nrOfFloors <= 0) {
@@ -68,25 +70,32 @@ public class Elevator {
         // update target if current floor == target and floor queue is not empty
         if (!primaryTargets.isEmpty()) {
 
-            // if not driving add target
-            if (committedDirection == IElevator.ELEVATOR_DIRECTION_UNCOMMITTED) {
-                setCommittedDirection(primaryTargets.first());
-                client.publishTargetFloor(id, primaryTargets.first());
+            if(debug){
+                System.out.println("ID:" + id + " " + primaryTargets);
+                System.out.println("Committed direction: " + id + " " + committedDirection);
+                System.out.println("Current Floor: " + id + " " + currentFloor);
+                System.out.println("Speed:" + id + " " + speed);
+                System.out.println("Doorstatus:" + id + " " + doorStatus);
+            }
 
+            // if not driving add target
+            if (committedDirection == IElevator.ELEVATOR_DIRECTION_UNCOMMITTED) {            
                 // set new target if elevator arrived at current target and doors are open
                 // current target is the lowest floor in the sorted target set if going up
-                if (currentFloor == primaryTargets.first()
-                    && speed == 0
-                    && (doorStatus == IElevator.ELEVATOR_DOORS_OPEN)) {
-                    
-                    // target handled -> remove it
-                    primaryTargets.remove(primaryTargets.first()); 
+                if (speed == 0 && doorStatus == IElevator.ELEVATOR_DOORS_OPEN) {
+                                                        
+                    if(currentFloor == primaryTargets.first()){                    
+                        // target handled -> remove it
+                        primaryTargets.remove(primaryTargets.first()); 
 
-                    // return if primary targets are empty now
-                    if (primaryTargets.isEmpty()) {
-                        // set committed direction to uncommitted
-                        setCommittedDirection(currentFloor);
-                        return;
+                        // return if primary targets are empty now
+                        if (primaryTargets.isEmpty()) {
+                            // set committed direction to uncommitted
+                            setCommittedDirection(currentFloor);
+                            return;
+                        }
+                        setCommittedDirection(primaryTargets.first());
+                        client.publishTargetFloor(id, primaryTargets.first());
                     }
                     setCommittedDirection(primaryTargets.first());
                     client.publishTargetFloor(id, primaryTargets.first());
@@ -97,17 +106,20 @@ public class Elevator {
             else if (committedDirection == IElevator.ELEVATOR_DIRECTION_UP) {
                 // always set nearest floor in committed direction as target
                 // might be update from button callbacks
-                setCommittedDirection(primaryTargets.first());
-                client.publishTargetFloor(id, primaryTargets.first());
+                // only update if elevator is moving --> doors are closed
+                if (doorStatus == IElevator.ELEVATOR_DOORS_CLOSED)
+                    client.publishTargetFloor(id, primaryTargets.first());
 
                 // set new target if elevator arrived at current target and doors are open
                 // current target is the lowest floor in the sorted target set if going up
                 if (currentFloor == primaryTargets.first()
                     && speed == 0
                     && (doorStatus == IElevator.ELEVATOR_DOORS_OPEN)) {
-
+                    
+                    if(debug)
+                        System.out.println("Remove target in dir up:" + primaryTargets.first());
                     // target handled -> remove it
-                    primaryTargets.remove(primaryTargets.first()); 
+                    primaryTargets.remove(primaryTargets.first());                     
 
                     // return if primary targets are empty now
                     if (primaryTargets.isEmpty()) {
@@ -124,8 +136,9 @@ public class Elevator {
             else if (committedDirection == IElevator.ELEVATOR_DIRECTION_DOWN) {
                 // always set nearest floor in committed direction as target
                 // might be update from button callbacks
-                setCommittedDirection(primaryTargets.last());
-                client.publishTargetFloor(id, primaryTargets.last());
+                // only update if elevator is moving --> doors are closed
+                if (doorStatus == IElevator.ELEVATOR_DOORS_CLOSED)
+                    client.publishTargetFloor(id, primaryTargets.last());
                 
                 // set new target if elevator arrived at current target and doors are open
                 // current target is the highest floor in the sorted target set if going down
@@ -133,8 +146,10 @@ public class Elevator {
                     && speed == 0
                     && (doorStatus == IElevator.ELEVATOR_DOORS_OPEN)) {
 
+                    if(debug)
+                        System.out.println("Remove target in dir down:" + primaryTargets.last());                    
                     // target handled -> remove it
-                    primaryTargets.remove(primaryTargets.last());
+                    primaryTargets.remove(primaryTargets.last());                    
 
                     // return if primary targets are empty now
                     if (primaryTargets.isEmpty()) {
@@ -196,6 +211,9 @@ public class Elevator {
         else {
             committedDirection = IElevator.ELEVATOR_DIRECTION_UNCOMMITTED;
         }
+        
+        if(debug)
+            System.out.println("Publish commited dir:" + committedDirection);
         client.publishCommittedDirection(id, committedDirection);
     }
 
